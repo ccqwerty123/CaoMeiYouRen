8fd8223722ccf4a2ebce46ac392627b2bf710641
ccqwerty123 <63049747+ccqwerty123@users.noreply.github.com>
2023-11-27 02:41:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ async function start() {
         return;
     }
     const items = [...blogResp.items.slice(0, 5)];
-    //const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
-    const text = items.map((e) => `${e.link}`).join('\n');
+    const text = items.map((e) => `${e.link}\n${getDescription(e)}`).join('\n');
     const readme = await fs.readFile('README.md', 'utf-8');
     const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
     await fs.writeFile('README.md', newReadme);
     console.log('更新博客链接成功');
+
+    function getDescription(item) {
+        const regex = /<description>\s*<!\[CDATA\[(.*?)]]>\s*<\/description>/s;
+        const match = item.content.match(regex);
+        if (match && match[1]) {
+            return match[1];
+        } else {
+            return '';
+        }
+    }
 }
 start();
